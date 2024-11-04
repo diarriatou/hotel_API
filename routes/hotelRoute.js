a59178703d3f82214a49a68c4e922c71f077eb20
Mame Diarra Ba <mamediarraba16@gmail.com>
2024-11-04 23:43:55 +0000

    affichage hotel selon l'utilisateur connecte

--- routes/hotelRoute.js
+++ routes/hotelRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../utils/uploadConfig');
-
+const authent = require('../middleware/auth');
 
 const {
   getHotels,
@@ -12,12 +12,12 @@ const {
 } = require('../controllers/hotelController');
 
 router.route('/')
-  .get(getHotels)
+  .get(authent.protect,getHotels)
   //.post(createHotel);
 
 router.route('/:id')
-  .get(getHotel)
-  .put(upload.single('photo'), updateHotel)
-  .delete(deleteHotel);
-router.post('/', upload.single('photo'), createHotel);
+  .get(authent.protect,getHotel)
+  .put(authent.protect,upload.single('photo'), updateHotel)
+  .delete(authent.protect,deleteHotel);
+router.post('/', authent.protect,upload.single('photo'), createHotel);
 module.exports = router;
